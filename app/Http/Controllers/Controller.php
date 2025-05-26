70b4000fcfaf1436391ba5329565305704d02b06
David Torres <dabiddo@gmail.com>
2025-05-26 21:54:50 +0000

    docs(company): open api for companies
    open api for companies index and store

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,16 @@ namespace App\Http\Controllers;
  *     version="1.0.0",
  *     description="Api Documentation"
  * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     in="header",
+ *     name="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     description="JWT Bearer Token"
+ * )
  */
 abstract class Controller
 {
--- app/Models/Company.php
+++ app/Models/Company.php
@@ -7,6 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @OA\Schema(
+ *     schema="Company",
+ *     type="object",
+ *     title="Company",
+ *     required={"id", "name","domain"},
+ *
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="name", type="string", example="Wayne Tech"),
+ *     @OA\Property(property="domain", type="string", example="R&D"),
+ *     @OA\Property(property="created_at", type="string", format="date-time", example="2023-01-01T00:00:00.000000Z"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time", example="2023-01-01T00:00:00.000000Z")
+ * )
+ */
 class Company extends Model
 {
     use HasFactory, SoftDeletes;
